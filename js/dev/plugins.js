7e45c87612678a2e96567576d590177c76a278e0
Robin Scott <robin.scott@comparethemarket.com>
2014-11-18 09:58:25 +0000

    Moved function.

--- js/dev/plugins.js
+++ js/dev/plugins.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
     var $offCanvasWrap = $(".off-canvas-wrap");
     console.log('ok');
     $(window).resize(function() {
-        if(this.outerWidth > 640 && $offCanvasWrap.hasClass("move-right")) {
+        if(this.outerWidth > 640 && $offCanvasWrap.hasClass("move-left")) {
             $(".exit-off-canvas").trigger("click");
         }
     });
--- js/dev/script.js
+++ js/dev/script.js
@@ -1,14 +1,3 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
-
-// Place any jQuery/helper plugins in here.
-$(document).ready(function() {
-    var $offCanvasWrap = $(".off-canvas-wrap");
-    console.log('ok');
-    $(window).resize(function() {
-        if(this.outerWidth > 640 && $offCanvasWrap.hasClass("move-left")) {
-            $(".exit-off-canvas").trigger("click");
-        }
-    });
-});
