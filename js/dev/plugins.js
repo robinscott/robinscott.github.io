26823bd5e1cc2ef9420e71cc1619be0b63e5e67b
Robin Scott <robin.scott@comparethemarket.com>
2014-11-18 12:52:30 +0000

    Added app script.

--- js/dev/plugins.js
+++ js/dev/plugins.js
@@ -1,32 +1,42 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+(function($) {
+    
+    var app = {};
 
-    while (length--) {
-        method = methods[length];
+    app.handleConsoleReporting = function() {
+        var method;
+        var noop = function () {};
+        var methods = [
+            'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+            'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+            'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+            'timeStamp', 'trace', 'warn'
+        ];
+        var length = methods.length;
+        var console = (window.console = window.console || {});
 
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
+        while (length--) {
+            method = methods[length];
+
+            // Only stub undefined methods.
+            if (!console[method]) {
+                console[method] = noop;
+            }
         }
     }
-}());
 
-// Place any jQuery/helper plugins in here.
-$(document).ready(function() {
-    var $offCanvasWrap = $(".off-canvas-wrap");
-    $(window).on('resize', Foundation.utils.throttle(function(e){
-        if(this.outerWidth > 640 && $offCanvasWrap.hasClass("move-left")) {
-            $(".exit-off-canvas").trigger("click");
-        }
-    }, 300));
-});
+    app.resizeClosesNavigation = function() {
+        $(document).ready(function() {
+            var $offCanvasWrap = $(".off-canvas-wrap");
+            $(window).on('resize', Foundation.utils.throttle(function(e){
+                if(this.outerWidth > 640 && $offCanvasWrap.hasClass("move-left")) {
+                    $(".exit-off-canvas").trigger("click");
+                }
+            }, 300));
+        });
+    }
+
+    return app;
+
+}(jQuery));
+
