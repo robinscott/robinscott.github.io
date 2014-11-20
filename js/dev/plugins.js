ef765bee11a67ac977e19fc63c852f2c60441100
Robin Scott <robin.scott@comparethemarket.com>
2014-11-20 12:44:05 +0000

    Mod scripts.

--- js/dev/plugins.js
+++ js/dev/plugins.js
@@ -28,8 +28,8 @@ var SiteScripts = (function($) {
 
     // Close the Foundation off canvas navigation when the window resizes
     app.resizeClosesNavigation = function() {
-        if (window.matchMedia(Foundation.media_queries.medium).matches) {
-          $(".off-canvas-wrap").addClass("move-right");
+        if (window.matchMedia(Foundation.media_queries.small).matches) {
+          $(".off-canvas-wrap").removeClass("move-right");
         };
 
         // var $offCanvasWrap = $(".off-canvas-wrap");
