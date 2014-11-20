e10389ccc2e05ae78a150e3df14aec46331f8c93
Robin Scott <robinscott@btinternet.com>
2014-11-20 13:26:11 +0000

    Updated script.js

--- js/dev/script.js
+++ js/dev/script.js
@@ -4,6 +4,9 @@ $(document).foundation({
   offcanvas : {
     // Sets method in which offcanvas opens.
     // [ move | overlap_single | overlap ]
-    open_method: 'overlap'
+    open_method: 'move', 
+    // Should the menu close when a menu link is clicked?
+    // [ true | false ]
+    close_on_click : true
   }
 });
