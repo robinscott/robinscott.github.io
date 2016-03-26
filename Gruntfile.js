3480e25ac28f323594fcf6fbebf1aacac39b276e
Robin Scott <webnibbler@gmail.com>
2016-03-26 22:05:17 +0000

    Revert "Moved to new design template."
    This reverts commit 5b3c20d830af6467c4807fd63660cf96bdaa294b.

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function (grunt) {
         },
         concurrent: {
             serve: [
-                'shell:jekyllBuild',
+                'shell:jekyllServe',
                 'browserSync',
                 'watch'
             ],
@@ -125,7 +125,7 @@ module.exports = function (grunt) {
                 files: [
                     '_layouts/**/*.html',
                     '_includes/**/*.html',
-                    'index.html'
+                    'index.md'
                 ],
                 tasks: ['jekyll:dev']
             }
